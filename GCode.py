ed2225728687c7e84c5ea6ab0618f36a7bbb791b
Ira Garg <111354438+Ira-2110993797@users.noreply.github.com>
2024-03-12 10:07:52 +0530

    Update GCode.py

--- GCode.py
+++ GCode.py
@@ -8,7 +8,7 @@ import sys
 
 if __name__=="__main__":
     if len(sys.argv)!= 2:
-        print("Usage: python. odel.py <name>")
+        print("Usage: python.model.py <name>")
         sys.exit(1)
         
     name=sys.argv[1]
