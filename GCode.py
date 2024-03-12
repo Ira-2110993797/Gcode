d698d04984b79a582780bf3b078756ba3de76668
Ira Garg <111354438+Ira-2110993797@users.noreply.github.com>
2024-03-12 09:53:12 +0530

    Add files via upload

---
+++ GCode.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+import sys
+
+if __name__=="__main__":
+    if len(sys.argv)!= 2:
+        print("Usage: python. odel.py <name>")
+        sys.exit(1)
+        
+    name=sys.argv[1]
+    print(f"{name}, welcome to docker!")
+
